92312c363173fa942a53c1ae122873bdbc01ae42
Yukun Guo <gyk.net@gmail.com>
2019-02-08 11:20:48 +0800

    bugfix: padding length cannot be 0

--- src/set2/challenge15/pkcs7_validate.jl
+++ src/set2/challenge15/pkcs7_validate.jl
@@ -4,7 +4,7 @@ struct InvalidPKCSPaddingException <: Exception end
 
 function pkcs7_remove!(data::Vector{UInt8})
     pad_len = data[end]
-    if pad_len <= length(data) && all((@view data[end - pad_len + 1 : end]) .== pad_len)
+    if 0 < pad_len <= length(data) && all((@view data[end - pad_len + 1 : end]) .== pad_len)
         resize!(data, length(data) - pad_len)
     else
         throw(InvalidPKCSPaddingException())
